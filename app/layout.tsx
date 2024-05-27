3ef4e7cbc6f0dda3d73d1bb480a2ad656d1b3264
Pavan Ranga <pavan.sv.ranga@gmail.com>
2024-05-27 13:21:37 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,19 +20,9 @@ export default function RootLayout({
       <body className={inter.className}>
         <div className="pb-16">{children}</div>
         <div className="fixed bottom-0 left-0 w-full  bg-slate-100 py-2">
-          <a
-            href="https://buildship.com/"
-            className="flex justify-center items-center"
-          >
-            <p className="text-secondary-foreground">Powered by </p>{" "}
-            <Image
-              src="/logo.webp"
-              width={25}
-              height={25}
-              alt="BuildShip logo"
-            />
-            <p>BuildShip</p>
-          </a>
+          
+            <p className="text-secondary-foreground">Pavan </p>
+            
         </div>
       </body>
     </html>
